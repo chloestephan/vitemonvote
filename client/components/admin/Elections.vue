175bea9f86b4e50ce625cd5145fbddd5308c6f59
Chloe Stephan <chloe.stephan@efrei.net>
2021-06-15 14:28:51 +0200

    détails css update

--- client/components/admin/Elections.vue
+++ client/components/admin/Elections.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="site-container">
+  <div>
+    <h2>LISTE DES ELECTIONS DEJA CRÉÉES</h2>
+    <hr>
     <div v-for="election in elections" :key="election.id" class="election">
       <router-link :to="chemin(maraude)">
         <h3>{{election.nom}}</h3>
@@ -31,4 +33,30 @@ module.exports = {
 </script>
 
 <style scoped>
+h2 {
+  font-family: 'open sans', 'HelveticaNeue', 'Helvetica Neue', 'Helvetica-Neue', Helvetica, Arial, sans-serif;
+  font-size: 28px;
+  line-height: 1.1em;
+  margin-bottom: 10px;
+  display: block;
+  font-size: 1.5em;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #001D6E;
+}
+
+hr {
+  display: block;
+  border: 0;
+  border-radius: 3em;
+  border-top: 2px solid #D60920;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 10%;
+  margin: auto;
+}
 </style>
